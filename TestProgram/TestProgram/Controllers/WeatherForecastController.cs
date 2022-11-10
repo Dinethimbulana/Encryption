3d16144a8c8b5ba1847ad6f706ab1e9be86f23a9
Dineth Imbulana <dcimbulana@outlook.com>
2022-11-10 15:55:40 +0530

    Upload

---
+++ TestProgram/TestProgram/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using EncryptData.Encryption;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProgram.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string method()
+        { 
+            byte[] encrypt = EncryptionMiddleware.Encrypt("Pa$$w0rd.");
+            string encryptValue = System.Text.Encoding.UTF8.GetString(encrypt);
+
+            string decryptValue = EncryptionMiddleware.DecryptString("�K.̮�\u0013rH����\u0014C�");
+
+            return decryptValue;
+        }
+    }
+}
---
+++ TestProgram/TestProgram/Encryption/EncryptionSettings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace EncryptData.Encryption
+{
+    public static class EncryptionSettings
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        public static string EncryptKey
+        {
+            get
+            {
+                return Configuration.GetValue<string>("EncryptionSettings:EncryptKey");
+            }
+        }
+        public static string EncryptIV
+        {
+            get
+            {
+                return Configuration.GetValue<string>("EncryptionSettings:EncryptIV");
+            }
+        }
+    }
+}
